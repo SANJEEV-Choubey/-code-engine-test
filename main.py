73a53a81fa0746279d0303bcd923584046887849
SANJEEV-Choubey <sanjeev.choubey@ibm.com>
2023-02-08 20:27:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='Hello World',
+              description='It is a hello world test for code engine ',
+              version=0.1,
+              docs_url='/docs',
+              redoc_url='/redoc')
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
